0693cceff96568768ee11ad29e9081b610c8e950
iona-b <ionabrabender@gmail.com>
2020-10-21 16:59:29 -0700

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,56 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// My Answer:
+
+// function reverse(str) {
+
+//     let newString  = "";
+
+//     for (let i = str.length - 1; i > -1; i--) {
+//         newString = newString.concat(str[i])
+//     }
+
+//     return newString;
+
+// }
+
+
+// Teaching Answer 1A:
+
+// function reverse(str) {
+
+//     let stringArray = str.split('')
+//     stringArray.reverse();
+//     return stringArray.join('');
+
+// }
+
+
+// Teaching Answer 1B:
+
+function reverse(str) {
+
+    return str.split('').reverse().join('');
+
+}
+
+
+// Teaching Answer 2:
+
+// function reverse(str) {
+
+
+
+// }
+
+
+// Teaching Answer 3:
+
+// function reverse(str) {
+
+
+
+// }
 
 module.exports = reverse;
