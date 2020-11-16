f9f9158d4d5f5081220c68f76a6ddc3026b1c82c
iona-b <ionabrabender@gmail.com>
2020-11-16 10:34:21 -0800

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,51 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// My Solution
+
+// function vowels(str) {
+
+//     let count = 0;
+
+//     let lowercase = str.toLowerCase();
+
+//     for (letter of lowercase) {
+//         if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
+//             count++;
+//         }
+//     }
+
+//     return count;
+
+// }
+
+
+// Solution 1
+
+// function vowels(str) {
+
+//     let counter = 0;
+//     const checker = ["a", "e", "i", "o", "u"];
+
+//     for (let char of str.toLowerCase()) {
+//         if (checker.includes(char)) {
+//             counter++;
+//         }
+//     }
+
+//     return counter;
+
+// }
+
+
+// Solution 2
+
+function vowels(str) {
+
+    const matches = str.match(/[aeiou]/gi);
+
+    return matches ? matches.length : 0;
+
+}
 
 module.exports = vowels;
