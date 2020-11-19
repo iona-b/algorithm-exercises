d370128a0f0fae731bd9838da2e0f27a0ee0a828
iona-b <ionabrabender@gmail.com>
2020-11-19 11:51:16 -0800

    levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,27 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+
+    let nodesArray = [root, "s"];
+    let counters = [0];
+
+    while (nodesArray.length > 1) {
+
+        let node = nodesArray.shift();
+
+        if (node !== "s") {
+            nodesArray.push(...node.children);
+            counters[counters.length - 1]++;
+        } else {
+            nodesArray.push("s");
+            counters.push(0);
+        }
+
+    }
+
+    return counters;
+
+}
 
 module.exports = levelWidth;
